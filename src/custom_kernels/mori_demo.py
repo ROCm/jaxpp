66074b57257f2c0fa0dfb67ac25b2f7cdbc4b29c
Chao <cchen104@amd.com>
2026-01-05 16:00:02 +0000

    Mori init (#4)
    * Add mori as third_party submodule
    
    * for demo

---
+++ src/custom_kernels/mori_demo.py
@@ -0,0 +1,11 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+import random
+# import mori
+import argparse, os, time
+from functools import partial
+
+# import torch
+# import torch.distributed as dist
